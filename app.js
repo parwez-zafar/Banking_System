92921febffd51b6cd84b6ce4a898ceac07535f53
parwez_zafar <parwezzafar3@gmail.com>
2021-08-21 20:35:16 +0530

    script adding

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 
 // cart table settings
 mongoose.connect("mongodb://localhost:27017/BankingSystem", {
